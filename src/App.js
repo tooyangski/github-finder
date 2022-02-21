46e0a34f68f1a8d16e4cc1e53ec4eac3aa6a4681
Aubrey Ivan Apungan <aubreyivanapungan@tooyangski.local>
2022-02-21 23:41:36 +0800

    added env variables for github api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<Notfound />} />
+            <Route path='/*' element={<Notfound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>About</h1>
+    </div>
+  );
+};
+
+export default About;
